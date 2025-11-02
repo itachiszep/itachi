584110f62dc5d70dc6d6b8e287de53cc4bdeb3b0
itachiszep <itachiszep@int.pl>
2025-11-02 22:15:53 +0100

    opis zmian

--- src/pages/music/music.jsx
+++ src/pages/music/music.jsx
@@ -5,15 +5,11 @@ export default function Home() {
   const videos = [
     "CGyEd0aKWZE?si=O3s69_Vd5LjzD2Tj",
     "BS46C2z5lVE?si=TFgIGA3HhPXOOJHm",
-   
   ];
 
   return (
-    <div className="bg-black text-white box-border m-0 p-0">
-      <div className="w-full bg-black flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 px-3 sm:px-4 md:px-6 lg:px-8 py-6 sm:py-8 md:py-10 lg:py-12 pt-20 md:pt-24 text-center">
-        
-        {/* Obraz */}
-       
+    <div className="w-full h-screen bg-black text-white box-border m-0 p-0 overflow-hidden">
+      <div className="w-full h-full bg-black flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 px-3 sm:px-4 md:px-6 lg:px-8 py-6 sm:py-8 md:py-10 lg:py-12 pt-20 md:pt-24 text-center overflow-y-auto">
         
         {/* Iframy jedno pod drugim */}
         <div className="w-full flex flex-col items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-4xl">
